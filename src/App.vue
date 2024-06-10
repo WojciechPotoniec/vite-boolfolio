08350ef1f102e82e756a72b1570d22cab23e998e
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-06-10 19:01:45 +0200

    create template with axios call to backend server laravel

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,37 @@
 <template>
-  
+  <h1>Welcome</h1>
+  <ul>
+    <li v-for="project in projects" :key="project.id">
+      {{ project.title }}
+      <img :src="store.imgBasePath + project.image" :alt="project.title">
+    </li>
+  </ul>
 </template>
 
 <script>
-  export default {
-    name: 'App'
-  }
+import { store } from "./store";
+import axios from "axios";
+export default {
+  name: "App",
+  data() {
+    return {
+      store,
+      projects: [],
+    };
+  },
+  methods: {
+    getAllProjects() {
+      axios.get(this.store.apiBaseUrl + "/projects").then((res) => {
+          console.log(res.data);
+          console.log(res.data.results.image);
+          this.projects = res.data.results;
+        })
+    },
+  },
+  mounted() {
+    this.getAllProjects();
+  },
+};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
