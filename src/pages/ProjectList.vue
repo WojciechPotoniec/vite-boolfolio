1e17b9e515bbb043433035748b605ac0091026f6
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-06-12 17:31:50 +0200

    fix

--- src/pages/ProjectList.vue
+++ src/pages/ProjectList.vue
@@ -1,41 +1,44 @@
 <template>
   <h1>Project List</h1>
-  <CardComponent />
-  <ul>
-    <li v-for="project in projects" :key="project.id">
-      {{ project.title }}
-      <img :src="store.imgBasePath + project.image" :alt="project.title" />
-    </li>
-  </ul>
+  <div class="row">
+    <div class="col-12 col-lg-6" v-for="project in projects" :key="project.id">
+      <CardComponent :card="project" />
+    </div>
+  </div>
 </template>
 
 <script>
 import CardComponent from "../components/CardComponent.vue";
 import { store } from "../store";
 import axios from "axios";
-    export default {
-        name: "ProjectList",
-        components: { 
-          CardComponent
-         },
-        data() {
+export default {
+  name: "ProjectList",
+  components: {
+    CardComponent,
+  },
+  data() {
     return {
       store,
       projects: [],
-    }
+      params: null
+    };
   },
   methods: {
     getAllProjects() {
-      axios.get(this.store.apiBaseUrl + "/projects").then((res) => {
+      axios
+        .get(this.store.apiBaseUrl + "/projects")
+        .then((res) => {
           console.log(res.data);
           this.projects = res.data.results;
-        }).catch((err) => {
+        })
+        .catch((err) => {
           // console.log(err);
           // console.log(err.response.data);
           this.$router.push({ name: "NotFound" });
-        }).finally(() => {
-          console.log("finally");
         })
+        .finally(() => {
+          console.log("finally");
+        });
     },
   },
   mounted() {
