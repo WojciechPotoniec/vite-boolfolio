4a48804e5e2c3dad484640c6023d7f825acbc4fa
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-06-13 15:44:45 +0200

    add new routes

--- src/router.js
+++ src/router.js
@@ -3,6 +3,8 @@ import { createRouter, createWebHistory } from 'vue-router';
 import HomeComponent from './pages/HomeComponent.vue';
 import ProjectList from './pages/ProjectList.vue';
 import ProjectComponent from './pages/ProjectComponent.vue';
+import AboutComponent from './pages/AboutComponent.vue';
+import ContactComponent from './pages/ContactComponent.vue';
 import NotFound from './pages/NotFound.vue';
 
 const router = createRouter({
@@ -22,6 +24,16 @@ const router = createRouter({
             path: '/project/:slug',
             name: 'project',
             component: ProjectComponent,
+        },
+        {
+            path: '/about',
+            name: 'about',
+            component: AboutComponent,
+        },
+        {
+            path: '/contact',
+            name: 'contact',
+            component: ContactComponent,
         },
         {
             path: '/:pathMatch(.*)*',
